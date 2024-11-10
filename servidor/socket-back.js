09bdba88a648fb49ff768168cf0297d95263a539
GeovanneOliver <geo-san@hotmail.com>
2024-11-10 17:51:00 -0300

    Fim da aula 2

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,11 +1,14 @@
+import "dotenv/config";
+
 import registrarEventosCadastros from "./registrarEventos/cadastros.js";
 import registrarEventosDocumento from "./registrarEventos/documento.js"
 import registrarEventosinicio from "./registrarEventos/inicio.js"
+import registrarEventosLogin from "./registrarEventos/login.js";
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
-  
-  registrarEventosinicio(socket, io);
-  registrarEventosDocumento(socket, io);
   registrarEventosCadastros(socket, io);
+  registrarEventosLogin(socket, io);
+  registrarEventosinicio(socket, io);
+  registrarEventosDocumento(socket, io);  
 });
